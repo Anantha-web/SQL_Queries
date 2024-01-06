1c5b0e1e352ae7a6c1ac0071ca8c46b496fca58a
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2024-01-06 17:24:18 +0530

    Create 1667. Fix Names in a Table.sql

---
+++ 1667. Fix Names in a Table.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1, 1)), LOWER(SUBSTR(name, 2, length(name)))) AS name
+FROM Users
+ORDER BY user_id;
+
+# CONCAT(A, B)
+# LOWER(str), UPPER(str)
+# SUBSTR(name, start_index, end_index)
+# length(name)
